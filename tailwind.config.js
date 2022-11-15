c489592c776379cc447b9e0534c7a50dfe5846e9
azmi72 <azmiali7221@gmail.com>
2022-11-16 03:06:51 +0700

    portfolio tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#f59e0b',
+        dark: '#0f172a',
+        grey: '#64748b',
+        silver: '#cbd5e1'
+      },
+      // screens: {
+      //   '2xl': '1320px',
+      // }
+    },
+  },
+  plugins: [],
+}
